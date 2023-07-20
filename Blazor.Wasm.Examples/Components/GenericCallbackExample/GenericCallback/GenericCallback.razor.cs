249b11df58cc8d82d75c139191a7908eb3c0e3bd
Andrew MacLeod <andrew.macleod@bjss.com>
2023-07-20 08:17:22 +0100

    Adds generic callback.

---
+++ Blazor.Wasm.Examples/Components/GenericCallbackExample/GenericCallback/GenericCallback.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Wasm.Examples.Components.GenericCallbackExample.GenericCallback;
+
+public class GenericCallbackComponent : ComponentBase
+{
+    [CascadingParameter(Name = "ExpectsAComponentImplementingThisInterface")]
+    public IExpectAComponentImplementingThisInterfaceFromMyLibrary? Parent { get; set; }
+
+    protected async Task InvokeCallbackAsync()
+    {
+        var someDataToSendToTheConsumingApplication = new SomeTypeFromMyLibrary
+        {
+            Data = "This is some data from the component in the component library."
+        };
+
+        await Parent.DoSomeStuffAsync(someDataToSendToTheConsumingApplication);
+    }
+}
+
+/// <summary>
+/// This would live in the component library.
+/// </summary>
+public interface IExpectAComponentImplementingThisInterfaceFromMyLibrary
+{
+    Task DoSomeStuffAsync(SomeTypeFromMyLibrary someTypeFromMyLibrary);
+}
+
+/// <summary>
+/// This would live in the component library - could be an interface, then the consumer
+/// would just create a type that implements it. 
+/// </summary>
+public class SomeTypeFromMyLibrary
+{
+    public string? Data { get; set; }
+}
---
+++ Blazor.Wasm.Examples/Components/GenericCallbackExample/WrapperInConsumingApp/WrapperInConsumingApp.razor.cs
@@ -0,0 +1,15 @@
+﻿using Blazor.Wasm.Examples.Components.GenericCallbackExample.GenericCallback;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Wasm.Examples.Components.GenericCallbackExample.WrapperInConsumingApp;
+
+public class WrapperInConsumingAppComponent : ComponentBase, IExpectAComponentImplementingThisInterfaceFromMyLibrary
+{
+    protected string? SomeDataSentBackToMe { get; set; }
+
+    public async Task DoSomeStuffAsync(SomeTypeFromMyLibrary someTypeFromMyLibrary)
+    {
+        SomeDataSentBackToMe = someTypeFromMyLibrary.Data;
+        await InvokeAsync(StateHasChanged);
+    }
+}
